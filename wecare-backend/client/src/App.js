1e89b9c62e1bebc43e28d6776e49d51e310fb03b
Nick Hsiao <hsiao.nicholas@gmail.com>
2019-09-25 21:15:24 -0700

    app.js update, home page can make call to express server

--- wecare-backend/client/src/App.js
+++ wecare-backend/client/src/App.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  //state = {users: []}
+class App extends Component{
 
-  /* componentDidMount() {
+  
+  state = {users: []}
+
+  componentDidMount() {
     fetch('/users')
     .then(res => res.json())
     .then(users => this.setState({users}));
-  } */
+  }
+render(){
 
-  return (
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users</h1>
+      <ul>
+        {this.state.users.map(user => <li key = {user.id}>{user.username} </li>)}
+      </ul>
     </div>
   );
-}
+}}
 
 export default App;
