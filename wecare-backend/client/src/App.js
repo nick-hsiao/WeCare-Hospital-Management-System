ddc38c7f2e2be43d1c2ac8860aef69c18ffe4ce5
Nick Hsiao <hsiao.nicholas@gmail.com>
2019-09-26 13:49:04 -0700

    added background and user list for home page

--- wecare-backend/client/src/App.js
+++ wecare-backend/client/src/App.js
@@ -1,6 +1,7 @@
-import React,{Component} from 'react';
+import React,{Component,Image,StyleSheet} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import backdrop from './img/hmsbackdrop.jpg'
 
 class App extends Component{
 
@@ -8,6 +9,7 @@ class App extends Component{
   state = {names: []}
 
   componentDidMount() {
+    console.log(backdrop);
     this.getNames();
   }
 
@@ -23,10 +25,26 @@ render(){
   const {names} = this.state;
 
  return (
+   <div style={{backgroundImage: `url(${backdrop})`,position: 'fixed',height:'100%',width: '100%',backgroundSize: 'cover',overflow: 'hidden'}}>
+    
     <div className="App">
-      {names.map(this.renderName)}
+      
+      <h1 style={{color: 'white',paddingBottom: 150}}> WeCare Hospital Management System</h1>
+      <div style={{fontSize: 25,color:'black'}}>
+        <h2>Users List</h2>
+        {names.map(this.renderName)}
+      </div>
     </div>
+  </div>
   );
+
+  
+
+
 }}
+
+
+
+
 
 export default App;
