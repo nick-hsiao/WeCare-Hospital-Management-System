24b5915e65545ea940d591cd41e4ea085d51d140
Nick Hsiao <hsiao.nicholas@gmail.com>
2019-09-26 01:06:56 -0700

    express query mysql added

--- wecare-backend/app.js
+++ wecare-backend/app.js
@@ -3,6 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
+var cors = require('cors');
+var port = process.env.PORT || 3001;
+
+var con = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'nickhsiao',
+  database : 'wecare',
+  //port: 3001,
+  //socketPath: '/private/tmp/mysql.sock'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,10 +41,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(cors());
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
+con.query('SELECT * from users', function (err, users, fields) {
+  if (err) throw err
+  console.log(users);
+
+})
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -37,5 +66,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+/* app.listen(port, () => {
+  console.log(`Listening on port ${port} `);
+}); */
 
 module.exports = app;
