ea9347646b030cb214bdb2642482be3ea9cc8a38
Huynh <huynhp24@gmail.com>
2019-10-15 01:49:16 -0700

    Final schema creation in MySQLCode.sql

--- MySqlCode.sql
+++ MySqlCode.sql
@@ -2,119 +2,119 @@ CREATE DATABASE HospitalManagementSystem;
 
 \u HospitalManagementSystem
 
+CREATE TABLE Patient(
+email varchar(50) PRIMARY KEY,
+password varchar(30) NOT NULL,
+name varchar(50) NOT NULL,
+address varchar(60) NOT NULL
+);
+
 CREATE TABLE MedicalHistory (
-uid int PRIMARY KEY, 
+uid int NOT NULL,
+email varchar(50) NOT NULL,
+PRIMARY KEY (uid, email),
+UNIQUE (uid, email),
 allergies varchar(50) NOT NULL, 
 gender varchar(20) NOT NULL, 
 conditions varchar(100) NOT NULL, 
 surgeries varchar(100) NOT NULL, 
-symptoms varchar(100) NOT NULL, 
-familyhistory varchar(100) NOT NULL, 
-medications varchar(100) NOT NULL, 
-habits varchar(100) NOT NULL 
-);
-
-CREATE TABLE User(
-email varchar(50) PRIMARY KEY,
- DOB varchar(30) NOT NULL,
- password varchar(30) NOT NULL
-);
-
-CREATE TABLE Patient(
-email varchar(50) PRIMARY KEY REFERENCES User (email),
-DOB varchar(30) REFERENCES User (DOB),
-password varchar(30) REFERENCES User (password),
-uid int UNIQUE NOT NULL,
-firstName varchar(25) NOT NULL,
-lastName varchar(25) NOT NULL,
-address varchar(60) NOT NULL
+medications varchar(100) NOT NULL,
+FOREIGN KEY (email) REFERENCES Patient (email)
 );
 
 CREATE TABLE DoctorNurse(
-email varchar(50) PRIMARY KEY REFERENCES User (email),
-DOB varchar(30) REFERENCES User (DOB),
-password varchar(30) REFERENCES User (password),
+email varchar(50) PRIMARY KEY,
+gender varchar(20),
+password varchar(30),
 uid int UNIQUE NOT NULL,
-firstName varchar(25) NOT NULL,
-lastName varchar(25) NOT NULL
+name varchar(50) NOT NULL
+);
+
+CREATE TABLE Building(
+name varchar(20) PRIMARY KEY,
+address varchar(30) NOT NULL
 );
 
 CREATE TABLE Room(
-room_id int PRIMARY KEY,
+id int AUTO_INCREMENT PRIMARY KEY,
+room_num int NOT NULL,
 floor int NOT NULL,
-bld_no int NOT NULL
-);
-
-
-CREATE TABLE DoctorsSeePatients(
-patient int NOT NULL,
-doctor int NOT NULL,
-concerns varchar(40) NOT NULL,
-symptoms varchar(40) NOT NULL,
-PRIMARY KEY (patient, doctor),
-FOREIGN KEY (patient) REFERENCES Patient (uid),
-FOREIGN KEY (doctor) REFERENCES DoctorNurse (uid)
+bldg varchar(20),
+UNIQUE (room_num, floor, bldg),
+status varchar(20),
+FOREIGN KEY (bldg) REFERENCES Building (name)
 );
 
 CREATE TABLE Appointment(
-appt_id int PRIMARY KEY,
+uid int PRIMARY KEY,
 date varchar(10) NOT NULL,
-time int NOT NULL,
+starttime int NOT NULL,
+endtime int NOT NULL,
 status varchar(15) NOT NULL
 );
 
-CREATE TABLE PatientsMakeHistory(
-patient int NOT NULL,
+CREATE TABLE PatientsViewAppointments(
+patient varchar(50) NOT NULL,
+appt int NOT NULL,
+concerns varchar(40) NOT NULL,
+symptoms varchar(40) NOT NULL,
+FOREIGN KEY (patient) REFERENCES Patient (email),
+FOREIGN KEY (appt) REFERENCES Appointment (uid)
+);
+
+
+CREATE TABLE Schedule(
+uid int PRIMARY KEY,
+starttime int NOT NULL,
+endtime int NOT NULL,
+breaktime int NOT NULL,
+daysOff varchar(20)
+);
+
+CREATE TABLE PatientsfillHistory(
+patient varchar(50) NOT NULL,
 medhistory int NOT NULL,
-PRIMARY KEY (patient,medhistory),
 UNIQUE (patient),
 UNIQUE (medhistory),
-FOREIGN KEY (patient) REFERENCES Patient (uid),
+FOREIGN KEY (patient) REFERENCES Patient (email),
+FOREIGN KEY (medhistory) REFERENCES MedicalHistory (uid)
+);
+
+CREATE TABLE Diagnose(
+appt int NOT NULL,
+patient varchar(50) NOT NULL,
+doctor varchar(50) NOT NULL,
+diagnosis varchar(40) NOT NULL,
+prescription varchar(20) NOT NULL,
+FOREIGN KEY (appt) REFERENCES Appointment (uid),
+FOREIGN KEY (patient) REFERENCES Patient (email),
+FOREIGN KEY (doctor) REFERENCES DoctorNurse (email)
+);
+
+CREATE TABLE ApptsToSchedules(
+appt int NOT NULL,
+sched int NOT NULL,
+FOREIGN KEY (appt) REFERENCES Appointment (uid),
+FOREIGN KEY (sched) REFERENCES Schedule (uid)
+);
+
+CREATE TABLE DocsHaveSchedules(
+sched int NOT NULL,
+doctor varchar(20) NOT NULL,
+FOREIGN KEY (sched) REFERENCES Schedule (uid),
+FOREIGN KEY (doctor) REFERENCES DoctorNurse (email)
+);
+
+CREATE TABLE DoctorViewsMed(
+medhistory int NOT NULL,
+doctor varchar(50) NOT NULL,
+FOREIGN KEY (doctor) REFERENCES DoctorNurse (email),
 FOREIGN KEY (medhistory) REFERENCES MedicalHistory (uid)
 );
 
 CREATE TABLE ApptInRoom(
 room int NOT NULL,
 appt int NOT NULL,
-FOREIGN KEY (room) REFERENCES Room (room_id),
-FOREIGN KEY (appt) REFERENCES Appointment (appt_id)
-);
-
-CREATE TABLE Diagnose(
-appt int NOT NULL,
-i int NOT NULL,
-PRIMARY KEY (appt,i),
-patient int NOT NULL,
-doctor int NOT NULL,
-FOREIGN KEY (appt) REFERENCES Appointment (appt_id),
-FOREIGN KEY (patient) REFERENCES Patient (uid),
-FOREIGN KEY (doctor) REFERENCES DoctorNurse (uid)
-);
-
-CREATE TABLE Scheduled(
-appt int NOT NULL,
-patient int NOT NULL,
-PRIMARY KEY (appt,patient),
-doctor int NOT NULL,
-FOREIGN KEY (appt) REFERENCES Appointment (appt_id),
-FOREIGN KEY (patient) REFERENCES Patient (uid),
-FOREIGN KEY (doctor) REFERENCES DoctorNurse (uid)
-);
-
-CREATE TABLE ScheduledFor(
-appt int NOT NULL,
-doctor int NOT NULL,
-PRIMARY KEY (appt,doctor),
-patient int NOT NULL,
-FOREIGN KEY (appt) REFERENCES Appointment (appt_id),
-FOREIGN KEY (patient) REFERENCES Patient (uid),
-FOREIGN KEY (doctor) REFERENCES DoctorNurse (uid)
-);
-
-CREATE TABLE DoctorsAccess(
-medhistory int NOT NULL,
-doctor int NOT NULL,
-PRIMARY KEY (medhistory,doctor),
-FOREIGN KEY (doctor) REFERENCES DoctorNurse (uid),
-FOREIGN KEY (medhistory) REFERENCES MedicalHistory (uid)
+FOREIGN KEY (room) REFERENCES Room (id),
+FOREIGN KEY (appt) REFERENCES Appointment (uid)
 );
