7f83217566c544c7d3d5da0d946f83bb64f657c2
diana sok <dianadesok98@gmail.com>
2019-12-02 18:35:51 -0800

    doc home page

--- wecare-backend/client/src/DocHome.js
+++ wecare-backend/client/src/DocHome.js
@@ -1,17 +1,19 @@
 import React, { Component, Image, StyleSheet, useState } from 'react';
 import logo from './logo.svg';
 import {
-  Box,
-  Button,
-  Heading,
-  Grommet,
-  Menu,
-  FormField,
-  TextInput,
-  Select,
-  grommet,
-  Grid,
-  Text
+    Box,
+    Button,
+    Heading,
+    Grommet,
+    Menu,
+    FormField,
+    TextInput,
+    Select,
+    grommet,
+    Grid,
+    Text,
+    Form,
+
 
 } from 'grommet';
 
@@ -19,170 +21,175 @@ import './App.css';
 import backdrop from './img/hmsbackdrop.jpg'
 
 const theme = {
-  global: {
-    colors: {
-      brand: '#00739D',
+    global: {
+        colors: {
+            brand: '#00739D',
+        },
+        font: {
+            family: 'Lato',
+        },
     },
-    font: {
-      family: 'Lato',
-    },
-  },
 };
 
 const SidebarButton = ({ label, ...rest }) => (
-  <Button plain {...rest}>
-    {({ hover }) => (
-      <Box
-        background={hover ? "#DADADA" : undefined}
-        pad={{ horizontal: "large", vertical: "medium" }}
-      >
-        <Text size="large">{label}</Text>
-      </Box>
-    )}
-  </Button>
+    <Button plain {...rest}>
+        {({ hover }) => (
+            <Box
+                background={hover ? "#DADADA" : undefined}
+                pad={{ horizontal: "large", vertical: "medium" }}
+            >
+                <Text size="large">{label}</Text>
+            </Box>
+        )}
+    </Button>
 );
 
 const SidebarButtons = () => {
-  const [active, setActive] = useState();
-  return (
-    <Grommet full theme={theme}>
-      <Box fill direction="row">
-        <Box background="brand">
-          {["Appointments", "Medical History","Settings"].map(label => (
-            <SidebarButton
-              key={label}
-              label={label}
-              active={label === active}
-              onClick={() => {
-                if (label === "Appointments"){
-                  window.location="/to do"
-                }
-                else if (label === "Medical History"){
-                  window.location="/MedHist"
-                }
-                else if (label === "Settings") {
-                  window.location="/to do"
-                }
-                setActive(label);
-              }}
-            />
-          ))}
-        </Box>
-      </Box>
-    </Grommet>
-  );
+    const [active, setActive] = useState();
+    return (
+        <Grommet full theme={theme}>
+            <Box fill direction="row">
+                <Box background="brand">
+                    {["Appointments", "Medical History", "Settings"].map(label => (
+                        <SidebarButton
+                            key={label}
+                            label={label}
+                            active={label === active}
+                            onClick={() => {
+                                if (label === "Appointments") {
+                                    window.location = "/to do"
+                                }
+                                else if (label === "Medical History") {
+                                    window.location = "/MedHist"
+                                }
+                                else if (label === "Settings") {
+                                    window.location = "/to do"
+                                }
+                                setActive(label);
+                            }}
+                        />
+                    ))}
+                </Box>
+            </Box>
+        </Grommet>
+    );
 };
 
 export class DocHome extends Component {
 
 
-  state = { names: [] }
+    state = { medhiststate: [] }
 
-  componentDidMount() {
-    // this.tryInsert();
-    this.getNames();
-    console.log(this.state.names);
-  }
+    componentDidMount() {
 
-  getNames = _ => {
-    fetch('http://localhost:3000/names') //does it have to fetch from the port we are listening on
-      .then(res => res.json())
-      .then(res => this.setState({ names: res.data }));
-  }
+        this.getNames("");
+        console.log(this.state.names);
+    }
 
-  renderName = ({ name, email }) => <div key={email}>{name} {name}</ div>
+    getNames(value) {
+        let patName = value;
+        console.log(patName);
+        fetch('http://localhost:3001/MedHistView?name=' + patName + '&variable=words')
+            .then(res => res.json())
+            .then(res => this.setState({ medhiststate: res.data }));
+    }
 
-  render() {
-    const { names } = this.state;
+    render() {
+        const { medhiststate } = this.state;
 
-    const Header = () => (
-      <Box
-        theme={theme}
-        tag='header'
-        background='#00739D'
-        pad='small'
-        elevation='small'
-        justify='between'
-        direction='row'
-        align='center'
-        flex={false}
-      >
-        <Heading level={3} margin='none'>
-          <strong>WeCare</strong>
-
-        </Heading>
-
-      </Box>
-    );
-
-    const Body = () => (
-        <div className="container">
-            <div className="panel panel-default p50 uth-panel">
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>name</th>
-                            <th>email</th>
-                            <th>address</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {names.map(patient =>
-                            <tr key={patient.id}>
-                                <td>{patient.name} </td>
-                                <td>{patient.email}</td>
-                                <td>{patient.address}</td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-);
-
-
-    return (
-      <Grommet full={true}>
-        <Box align="left">
-          <Header />
-          <Grid
-            fill
-            rows={['auto', 'flex']}
-            columns={['auto', 'flex']}
-            areas={[
-              { name: 'sidebar', start: [0, 1], end: [0, 1] },
-              { name: 'main', start: [1, 1], end: [1, 1] },
-            ]}>
+        const Header = () => (
             <Box
-              gridArea="sidebar"
-              width="small"
-              animation={[
-                { type: 'fadeIn', duration: 300 },
-                { type: 'slideRight', size: 'xlarge', duration: 150 },
-              ]}
+                tag='header'
+                background='brand'
+                pad='small'
+                elevation='small'
+                justify='between'
+                direction='row'
+                align='center'
+                flex={false}
             >
-              <SidebarButtons />
+                <Heading level={3} margin='none'>
+                    <strong>WeCare</strong>
+                </Heading>
+
             </Box>
-            <Box
-              gridArea="main"
-              justify="center"
-              align="center">
-              <Body />
-            </Box>
-          </Grid>
+        );
+
+        const Body = () => (
+            <div className="container">
+                <div className="panel panel-default p50 uth-panel">
+                    <table className="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>name</th>
+                                <th>email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {medhiststate.map(patient =>
+                                <tr key={patient.id}>
+                                    <td>{patient.Name} </td>
+                                    <td>{patient.UID}
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        );
+
+        return (
+            <Grommet full={true}
+            theme = {theme}>
+                <Box align="left">
+                    <Header />
+                    <Grid
+                        fill
+                        rows={['auto', 'flex']}
+                        columns={['auto', 'flex']}
+                        areas={[
+                            { name: 'sidebar', start: [0, 1], end: [0, 1] },
+                            { name: 'main', start: [1, 1], end: [1, 1] },
+                        ]}>
+                        <Box
+                            gridArea="sidebar"
+                            width="small"
+                            animation={[
+                                { type: 'fadeIn', duration: 300 },
+                                { type: 'slideRight', size: 'xlarge', duration: 150 },
+                            ]}
+                        >
+                            <SidebarButtons />
+                        </Box>
+                        <Box
+                            gridArea="main"
+                            justify="center"
+                            align="center">
+                            <Form
+                                onSubmit={({ value }) => {
+                                    console.log("Submit", value);
+                                    this.getNames(value.email);
+                                }}>
+                                <FormField name="email" label="Name" />
+                                <Button type="submit" primary label="Submit" />
+                            </Form>
+                            <Body />
+                        </Box>
+                    </Grid>
 
 
 
 
-        </Box>
-      </Grommet>
-    );
+                </Box>
+            </Grommet>
+        );
 
 
 
 
-  }
+    }
 }
 
 export default DocHome;
